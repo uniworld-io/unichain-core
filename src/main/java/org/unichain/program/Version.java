daa62f0dcf7d563ec7c63241cd32b97bf8f49ffa
johnwick <sonnv18.bigdata@gmail.com>
2021-06-19 00:17:01 +0700

    - update Version to 2.0.0

--- src/main/java/org/unichain/program/Version.java
+++ src/main/java/org/unichain/program/Version.java
@@ -1,10 +1,9 @@
 package org.unichain.program;
 
 public class Version {
-
-  private static final String version = "1.0.1";
-  public static final String versionName = "unichain-1.0.1";
-  public static final String versionCode = "101";
+  private static final String version = "2.0.0";
+  public static final String versionName = "unichain-2.0.0";
+  public static final String versionCode = "200";
 
   public static String getVersion() {
     return version;

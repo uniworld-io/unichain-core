3472e7292531a2a7b232140111ac4984a896e3ef
Elon Kusk <sonnv18.bigdata@gmail.com>
2021-07-29 13:44:06 +0700

    - to v2.0.1

--- src/main/java/org/unichain/program/Version.java
+++ src/main/java/org/unichain/program/Version.java
@@ -1,8 +1,8 @@
 package org.unichain.program;
 
 public class Version {
-  private static final String version = "2.0.0";
-  public static final String versionName = "unichain-2.0.0";
+  private static final String version = "2.0.1";
+  public static final String versionName = "unichain-2.0.1";
   public static final String versionCode = "200";
 
   public static String getVersion() {

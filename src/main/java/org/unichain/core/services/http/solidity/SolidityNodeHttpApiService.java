9348c98a856e0026467d0833c4595cf67468e6c5
Elon Kusk <sonnv18.bigdata@gmail.com>
2021-08-11 15:10:53 +0700

    - fix walletsolidity path [getfuturetoken]

--- src/main/java/org/unichain/core/services/http/solidity/SolidityNodeHttpApiService.java
+++ src/main/java/org/unichain/core/services/http/solidity/SolidityNodeHttpApiService.java
@@ -105,7 +105,7 @@ public class SolidityNodeHttpApiService implements Service {
       // same as FullNode
       context.addServlet(new ServletHolder(getAccountServlet), "/walletsolidity/getaccount");
       context.addServlet(new ServletHolder(getTokenPoolServlet), "/walletsolidity/gettokenpool");
-      context.addServlet(new ServletHolder(getTokenFutureServlet), "/getfuturetoken");
+      context.addServlet(new ServletHolder(getTokenFutureServlet), "/walletsolidity/getfuturetoken");
       context.addServlet(new ServletHolder(listWitnessesServlet), "/walletsolidity/listwitnesses");
       context.addServlet(new ServletHolder(getAssetIssueListServlet), "/walletsolidity/getassetissuelist");
       context.addServlet(new ServletHolder(getPaginatedAssetIssueListServlet), "/walletsolidity/getpaginatedassetissuelist");

d52a371edfdd49fef4a2c762aa60ce33652ecc77
elonmusk <elonkusk@gmail.com>
2021-12-11 15:10:03 +0700

    - update urc 30 params

--- src/main/java/org/unichain/core/actuator/TokenCreateActuatorV3.java
+++ src/main/java/org/unichain/core/actuator/TokenCreateActuatorV3.java
@@ -61,7 +61,7 @@ public class TokenCreateActuatorV3 extends AbstractActuator {
 
       if(!ctx.hasField(TOKEN_CREATE_FIELD_END_TIME))
       {
-        capsule.setEndTime(startTime + Parameter.ChainConstant.DEFAULT_TOKEN_AGE);
+        capsule.setEndTime(startTime + DEFAULT_TOKEN_AGE_V3);
       }
 
       capsule.setBurnedToken(0L);
@@ -110,9 +110,9 @@ public class TokenCreateActuatorV3 extends AbstractActuator {
       var startTime = ctx.hasField(TOKEN_CREATE_FIELD_START_TIME) ? ctx.getStartTime() : dbManager.getHeadBlockTimeStamp();
       var maxTokenActive = dbManager.getHeadBlockTimeStamp() + MAX_TOKEN_ACTIVE;
       Assert.isTrue((startTime >= dbManager.getHeadBlockTimeStamp()) && (startTime <= maxTokenActive), "Invalid start time: must be greater than current block time and lower than limit timestamp:" +maxTokenActive);
+      long endTime = ctx.hasField(TOKEN_CREATE_FIELD_END_TIME) ? ctx.getEndTime() : (startTime + DEFAULT_TOKEN_AGE_V3);
+      long maxTokenAge = dbManager.getHeadBlockTimeStamp() + MAX_TOKEN_AGE_V3;
 
-      var endTime = ctx.hasField(TOKEN_CREATE_FIELD_END_TIME) ? ctx.getEndTime() : (startTime + Parameter.ChainConstant.DEFAULT_TOKEN_AGE);
-      var maxTokenAge = dbManager.getHeadBlockTimeStamp() + MAX_TOKEN_AGE;
       Assert.isTrue((endTime > 0)
               && (endTime > startTime )
               && (endTime > dbManager.getHeadBlockTimeStamp())
@@ -124,7 +124,7 @@ public class TokenCreateActuatorV3 extends AbstractActuator {
       Assert.isTrue(ctx.getFee() >= 0 && ctx.getFee() <= TOKEN_MAX_TRANSFER_FEE, "Invalid token transfer fee: must be positive and not exceed max fee : " + TOKEN_MAX_TRANSFER_FEE + " tokens");
       Assert.isTrue(ctx.getExtraFeeRate() >= 0 && ctx.getExtraFeeRate() <= 100 && ctx.getExtraFeeRate() <= TOKEN_MAX_TRANSFER_FEE_RATE, "Invalid extra fee rate , should between [0, " + TOKEN_MAX_TRANSFER_FEE_RATE + "]");
 
-      Assert.isTrue(ctx.getFeePool() >= 0 && (accountCap.getBalance() >= calcFee() + ctx.getFeePool()), "Invalid fee pool or not enough balance for fee & pre-deposit pool fee");
+      Assert.isTrue(ctx.getFeePool() >= MIN_TOKEN_POOL_FEE && (accountCap.getBalance() >= calcFee() + ctx.getFeePool()), "Invalid fee pool or not enough balance for fee & pre-deposit pool fee");
       Assert.isTrue(ctx.getLot() >= 0, "Invalid lot: must not negative");
       Assert.isTrue(ctx.getExchUnxNum() > 0, "Invalid exchange unw number: must be positive");
       Assert.isTrue(ctx.getExchNum() > 0, "Invalid exchange token number: must be positive");

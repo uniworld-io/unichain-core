f676c2e7db034450e3f35449482e5473197953f9
AbrahamNguyen26 <95601983+AbrahamNguyen26@users.noreply.github.com>
2021-12-18 22:02:05 +0700

    - fix coding convention: WitnessUpdateActuator

--- src/main/java/org/unichain/core/actuator/WitnessUpdateActuator.java
+++ src/main/java/org/unichain/core/actuator/WitnessUpdateActuator.java
@@ -4,9 +4,11 @@ import com.google.protobuf.Any;
 import com.google.protobuf.ByteString;
 import com.google.protobuf.InvalidProtocolBufferException;
 import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+import lombok.var;
+import org.springframework.util.Assert;
 import org.unichain.core.Wallet;
 import org.unichain.core.capsule.TransactionResultCapsule;
-import org.unichain.core.capsule.WitnessCapsule;
 import org.unichain.core.capsule.utils.TransactionUtil;
 import org.unichain.core.db.Manager;
 import org.unichain.core.exception.BalanceInsufficientException;
@@ -24,11 +26,11 @@ public class WitnessUpdateActuator extends AbstractActuator {
 
   @Override
   public boolean execute(TransactionResultCapsule ret) throws ContractExeException {
-    long fee = calcFee();
+    var fee = calcFee();
     try {
-      final WitnessUpdateContract witnessUpdateContract = this.contract.unpack(WitnessUpdateContract.class);
-      byte[] ownerAddress = witnessUpdateContract.getOwnerAddress().toByteArray();
-      WitnessCapsule witnessCapsule = this.dbManager.getWitnessStore().get(ownerAddress);
+      val witnessUpdateContract = this.contract.unpack(WitnessUpdateContract.class);
+      var ownerAddress = witnessUpdateContract.getOwnerAddress().toByteArray();
+      var witnessCapsule = this.dbManager.getWitnessStore().get(ownerAddress);
       witnessCapsule.setUrl(witnessUpdateContract.getUpdateUrl().toStringUtf8());
       this.dbManager.getWitnessStore().put(witnessCapsule.createDbKey(), witnessCapsule);
       chargeFee(ownerAddress, fee);
@@ -43,15 +45,10 @@ public class WitnessUpdateActuator extends AbstractActuator {
 
   @Override
   public boolean validate() throws ContractValidateException {
-    if (this.contract == null) {
-      throw new ContractValidateException("No contract!");
-    }
-    if (this.dbManager == null) {
-      throw new ContractValidateException("No dbManager!");
-    }
-    if (!this.contract.is(WitnessUpdateContract.class)) {
-      throw new ContractValidateException("contract type error,expected type [WitnessUpdateContract],real type[" + contract.getClass() + "]");
-    }
+    Assert.notNull(this.contract, "No contract!");
+    Assert.notNull(this.dbManager, "No dbManager!");
+    Assert.isTrue(this.contract.is(WitnessUpdateContract.class), "contract type error,expected type [WitnessUpdateContract],real type[" + contract.getClass() + "]");
+
     final WitnessUpdateContract contract;
     try {
       contract = this.contract.unpack(WitnessUpdateContract.class);
@@ -60,22 +57,14 @@ public class WitnessUpdateActuator extends AbstractActuator {
       throw new ContractValidateException(e.getMessage());
     }
 
-    byte[] ownerAddress = contract.getOwnerAddress().toByteArray();
-    if (!Wallet.addressValid(ownerAddress)) {
-      throw new ContractValidateException("Invalid address");
-    }
+    var ownerAddress = contract.getOwnerAddress().toByteArray();
+    Assert.isTrue(Wallet.addressValid(ownerAddress), "Invalid address");
 
-    if (!this.dbManager.getAccountStore().has(ownerAddress)) {
-      throw new ContractValidateException("account does not exist");
-    }
+    Assert.isTrue(this.dbManager.getAccountStore().has(ownerAddress), "account does not exist");
 
-    if (!TransactionUtil.validUrl(contract.getUpdateUrl().toByteArray())) {
-      throw new ContractValidateException("Invalid url");
-    }
+    Assert.isTrue(TransactionUtil.validUrl(contract.getUpdateUrl().toByteArray()), "Invalid url");
 
-    if (!this.dbManager.getWitnessStore().has(ownerAddress)) {
-      throw new ContractValidateException("Witness does not exist");
-    }
+    Assert.isTrue(this.dbManager.getWitnessStore().has(ownerAddress), "Witness does not exist");
 
     return true;
   }

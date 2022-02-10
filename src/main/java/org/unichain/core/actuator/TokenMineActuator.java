1e8a1c1bf6746f023129cf6f06a1c017007eab81
elonkusk <sonnv18.bigdata@gmail.com>
2022-02-10 15:56:39 +0700

    - fix TokenMineActuator safely computing math

--- src/main/java/org/unichain/core/actuator/TokenMineActuator.java
+++ src/main/java/org/unichain/core/actuator/TokenMineActuator.java
@@ -47,8 +47,7 @@ public class TokenMineActuator extends AbstractActuator {
       var ctx = contract.unpack(MineTokenContract.class);
       var tokenKey = Util.stringAsBytesUppercase(ctx.getTokenName());
       var tokenCapsule = dbManager.getTokenPoolStore().get(tokenKey);
-      //@todo safely doing math compute
-      tokenCapsule.setTotalSupply(tokenCapsule.getTotalSupply() + ctx.getAmount());
+      tokenCapsule.setTotalSupply(Math.addExact(tokenCapsule.getTotalSupply(), ctx.getAmount()));
       dbManager.getTokenPoolStore().put(tokenKey, tokenCapsule);
 
       var ownerAddress = ctx.getOwnerAddress().toByteArray();
@@ -92,8 +91,7 @@ public class TokenMineActuator extends AbstractActuator {
       Assert.isTrue(ctx.getAmount() >= tokenPool.getLot(), "Mined amount at least equal lot: " + tokenPool.getLot());
 
       // avail to mine = max - total - burned
-      //@todo safely doing math compute
-      var availableToMine = tokenPool.getMaxSupply() - tokenPool.getTotalSupply() - tokenPool.getBurnedToken();
+      var availableToMine = Math.subtractExact(tokenPool.getMaxSupply(), Math.addExact(tokenPool.getTotalSupply(), tokenPool.getBurnedToken()));
       Assert.isTrue(ctx.getAmount() <= availableToMine, "Not enough frozen token to mine, maximum allowed: " + availableToMine);
 
       return true;

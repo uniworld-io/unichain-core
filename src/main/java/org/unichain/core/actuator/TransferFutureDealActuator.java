b6c3428192f2710c8a73173b753931d58bf73ef3
elonkusk <sonnv18.bigdata@gmail.com>
2022-06-09 18:51:18 +0700

    reivew TransferFutureDealActuator

--- src/main/java/org/unichain/core/actuator/TransferFutureDealActuator.java
+++ src/main/java/org/unichain/core/actuator/TransferFutureDealActuator.java
@@ -8,7 +8,6 @@ import lombok.var;
 import org.springframework.util.Assert;
 import org.unichain.common.utils.ActuatorUtil;
 import org.unichain.core.Wallet;
-import org.unichain.core.capsule.AccountCapsule;
 import org.unichain.core.capsule.TransactionResultCapsule;
 import org.unichain.core.config.Parameter;
 import org.unichain.core.db.Manager;
@@ -19,8 +18,8 @@ import org.unichain.protos.Contract;
 import org.unichain.protos.Protocol;
 
 import java.util.Arrays;
-import java.util.Objects;
 
+//@todo add amount to break future deal to smaller deals
 @Slf4j(topic = "actuator")
 public class TransferFutureDealActuator extends AbstractActuator {
   public TransferFutureDealActuator(Any contract, Manager dbManager) {
@@ -31,19 +30,21 @@ public class TransferFutureDealActuator extends AbstractActuator {
   public boolean execute(TransactionResultCapsule ret) throws ContractExeException {
     var fee = calcFee();
     try {
+      var accountStore = dbManager.getAccountStore();
+
       var ctx = contract.unpack(Contract.FutureDealTransferContract.class);
       var ownerAddress = ctx.getOwnerAddress().toByteArray();
       var toAddress = ctx.getToAddress().toByteArray();
-      var toAccount = dbManager.getAccountStore().get(toAddress);
-      if (Objects.isNull(toAccount)) {
-        var withDefaultPermission = (dbManager.getDynamicPropertiesStore().getAllowMultiSign() == 1);
-        toAccount = new AccountCapsule(ByteString.copyFrom(toAddress), Protocol.AccountType.Normal, dbManager.getHeadBlockTimeStamp(), withDefaultPermission, dbManager);
-        dbManager.getAccountStore().put(toAddress, toAccount);
+      var dealId = ctx.getDealId();
+
+      if (!accountStore.has(toAddress)) {
+        createDefaultAccount(toAddress);
         fee = Math.addExact(fee, dbManager.getDynamicPropertiesStore().getCreateNewAccountFeeInSystemContract());
       }
-      var dealId = ctx.getDealId();
-      chargeFee(ownerAddress, fee);
+
       transferFutureDeal(ownerAddress, toAddress, dealId);
+
+      chargeFee(ownerAddress, fee);
       ret.setStatus(fee, Protocol.Transaction.Result.code.SUCESS);
       return true;
     } catch (Exception e) {
@@ -62,16 +63,11 @@ public class TransferFutureDealActuator extends AbstractActuator {
 
       var ctx = this.contract.unpack(Contract.FutureDealTransferContract.class);
       var ownerAddress = ctx.getOwnerAddress().toByteArray();
-      Assert.isTrue(Wallet.addressValid(ownerAddress), "Invalid ownerAddress");
-
       var toAddress = ctx.getToAddress().toByteArray();
+      Assert.isTrue(Wallet.addressValid(ownerAddress), "Invalid ownerAddress");
       Assert.isTrue(Wallet.addressValid(toAddress), "Invalid toAddress");
 
-      var maxExpireTime = Math.addExact(dbManager.getHeadBlockTimeStamp(), dbManager.getMaxFutureTransferTimeDurationUnwV3());
-      Assert.isTrue((ctx.getDealId() > dbManager.getHeadBlockTimeStamp()) && (ctx.getDealId() <= maxExpireTime),
-              "deal id must greater current block time, lower than maximum timestamp:" + maxExpireTime);
-
-      Assert.isTrue(!Arrays.equals(toAddress, ownerAddress), "Cannot transfer unw to yourself");
+      Assert.isTrue(!Arrays.equals(toAddress, ownerAddress), "Cannot transfer to yourself");
 
       var ownerAccount = dbManager.getAccountStore().get(ownerAddress);
       Assert.notNull(ownerAccount, "no OwnerAccount found");
@@ -82,19 +78,17 @@ public class TransferFutureDealActuator extends AbstractActuator {
         fee = Math.addExact(fee, dbManager.getDynamicPropertiesStore().getCreateNewAccountFeeInSystemContract());
       }
 
-      /*
-      Check if owner doesn't have locked tick
+      /**
+        Check if owner doesn't have locked tick
       */
       var tickDay = Util.makeDayTick(ctx.getDealId());
       var tickKey = Util.makeFutureTransferIndexKey(ownerAddress, tickDay);
       var futureStore = dbManager.getFutureTransferStore();
       Assert.isTrue(futureStore.has(tickKey), "OwnerAddress doesn't have future locked with deal id " + ctx.getDealId());
 
-      //after UvmSolidity059 proposal, send unx to smartContract by actuator is not allowed.
-      var transferToSmartContract = (dbManager.getDynamicPropertiesStore().getAllowUvmSolidity059() == 1)
-              && toAccount != null
-              && toAccount.getType() == Protocol.AccountType.Contract;
-      Assert.isTrue(!transferToSmartContract, "Cannot transfer/self-lock unw to/by smartContract.");
+      if(toAccount != null){
+        Assert.isTrue(toAccount.getType() == Protocol.AccountType.Normal, "Transfer to normal account only");
+      }
 
       var balance = ownerAccount.getBalance();
       Assert.isTrue(balance > fee, "Validate TransferContract error, balance is not sufficient");
@@ -121,11 +115,12 @@ public class TransferFutureDealActuator extends AbstractActuator {
     var tickKey = Util.makeFutureTransferIndexKey(ownerAddress, tickDay);
 
     var futureStore = dbManager.getFutureTransferStore();
-    var tick = futureStore.get(tickKey);
+    var futureDeal = futureStore.get(tickKey);
 
-    ActuatorUtil.removeFutureDeal(dbManager, ownerAddress, tick);
-    ActuatorUtil.addFutureDeal(dbManager, toAddress, tick.getBalance(), dealId);
-    // remove old tickKey
+    //@todo review
+    ActuatorUtil.removeFutureDeal(dbManager, ownerAddress, futureDeal);
+    ActuatorUtil.addFutureDeal(dbManager, toAddress, futureDeal.getBalance(), dealId);
+
     futureStore.delete(tickKey);
   }
 }

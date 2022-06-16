c89a2b14ab4e5233ff8084513544e443d29704b8
elonkusk <sonnv18.bigdata@gmail.com>
2022-06-16 18:55:31 +0700

    release 2.0.7

--- src/main/java/org/unichain/program/Version.java
+++ src/main/java/org/unichain/program/Version.java
@@ -1,9 +1,9 @@
 package org.unichain.program;
 
 public class Version {
-  private static final String version = "2.0.6";
-  public static final String versionName = "unichain-2.0.6";
-  public static final String versionCode = "206";
+  private static final String version = "2.0.7";
+  public static final String versionName = "unichain-2.0.7";
+  public static final String versionCode = "207";
 
   public static String getVersion() {
     return version;
